48128477f7d6390c9338896673d774e24823f7f9
charsipandaa <154540077+charsipandaa@users.noreply.github.com>
2023-12-25 22:21:13 +0500

    Add files via upload

---
+++ a.asm
@@ -0,0 +1,27 @@
+[org 0x0100]
+jmp start
+multiplicand: dd 1200 
+multiplier: dw 400
+result: dd 0
+
+start: 
+mov cl, 16
+mov dx, [multiplier]
+
+checkbit: 
+shr dx, 1
+jnc skip
+
+mov ax, [multiplicand]
+add [result], ax
+mov ax, [multiplicand+2]
+adc [result+2], ax
+
+skip: 
+shl word[multiplicand], 1
+rcl word[multiplicand+2], 1
+dec cl
+jnz checkbit
+
+mov ax, 0x4c00
+int 0x21
